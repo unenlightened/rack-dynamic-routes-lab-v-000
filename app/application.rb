74ed5ee8a503798eb94598f9d73d82c4e02c0715
Chely Ho <adept_inept@yahoo.com>
2018-03-06 16:46:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Response.new(env)
+    req = Rack::Request.new(env)
 
      if req.path == "/items"
     #if req.path.match(/items/)
