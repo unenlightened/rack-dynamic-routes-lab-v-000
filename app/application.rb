f8e04d63536cd61f45922f8db998c87486d76bd9
Chely Ho <adept_inept@yahoo.com>
2018-03-06 16:45:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Response.new(env)
 
-    if req.path.match(/items/)
+     if req.path == "items"
+    #if req.path.match(/items/)
       item_search = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_search}
 
