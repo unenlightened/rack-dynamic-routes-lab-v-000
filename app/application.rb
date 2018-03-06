405f521b54da7429173b58aade23df23c2bea586
Chely Ho <adept_inept@yahoo.com>
2018-03-06 16:30:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,16 @@
 class application
 
   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Response.new(env)
 
+    if req.path=="/items"
 
+    else
+      resp.write "Route not found"
+      resp.statu = 404
+    end
 
+    resp.finish
   end
 end
